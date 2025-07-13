244540798a52ed55ab03b6353a36566d3d044bd2
dabila <mboya4194@gmail.com>
2025-07-13 18:52:15 +0300

    Update garch_model.py

--- model/garch_model.py
+++ model/garch_model.py
@@ -1,16 +1,13 @@
+
 from arch import arch_model
 import pandas as pd
 
 def forecast_garch_var(df, p=1, q=1):
-    returns = df['Returns'].dropna() * 100  # Scale to percentage
-
+    returns = df['Returns'].dropna() * 100
     am = arch_model(returns, vol='Garch', p=p, q=q)
     res = am.fit(disp='off')
-
     forecast = res.forecast(horizon=5)
     vol_forecast = forecast.variance.values[-1, :] ** 0.5
-    var_1d = res.conditional_volatility[-1] * 1.65  # 95% VaR
-
+    var_1d = res.conditional_volatility[-1] * 1.65
     forecast_df = pd.Series(vol_forecast, name="Volatility Forecast")
-
     return forecast_df, var_1d
